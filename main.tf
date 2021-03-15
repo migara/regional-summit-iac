7f9cb8dec93cb8585504117cb6bd47df8aa88982
Migara Ekanayake <2110772+migara@users.noreply.github.com>
2021-03-15 17:24:58 +0000

    Add second VPC

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ resource "aws_vpc" "shared" {
   }
 }
 
-# locals {
-#   config = yamldecode(file("./env/${terraform.workspace}/config.yml"))
-# }
+locals {
+  config = yamldecode(file("./env/${terraform.workspace}/config.yml"))
+}
 
-# resource "aws_vpc" "this" {
-#   cidr_block = local.config.cidr_block
-#   tags       = merge(var.tags, local.config.tags, { Name = "${local.config.prefix} VPC"})
-# }
+resource "aws_vpc" "this" {
+  cidr_block = local.config.cidr_block
+  tags       = merge(var.tags, local.config.tags, { Name = "${local.config.prefix} VPC"})
+}
