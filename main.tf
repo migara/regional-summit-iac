709575e0366587023a2e84f8b281d68522790a03
Migara Ekanayake <2110772+migara@users.noreply.github.com>
2021-03-15 11:14:59 +0000

    add VPC to dev

--- main.tf
+++ main.tf
@@ -4,5 +4,5 @@ locals {
 
 resource "aws_vpc" "this" {
   cidr_block = local.config.cidr_block
-  tags       = merge(var.tags, local.config.tags)
+  tags       = merge(var.tags, local.config.tags, { Name = "${local.config.prefix} VPC"})
 }
