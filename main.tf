e863b71bb0cf9e37427da324393b6cfa1143dbbc
Migara Ekanayake <2110772+migara@users.noreply.github.com>
2021-03-15 12:18:19 +0000

    add shared VPC

--- main.tf
+++ main.tf
@@ -2,7 +2,13 @@ locals {
   config = yamldecode(file("./env/${terraform.workspace}/config.yml"))
 }
 
-resource "aws_vpc" "this" {
-  cidr_block = local.config.cidr_block
-  tags       = merge(var.tags, local.config.tags, { Name = "${local.config.prefix} VPC"})
+
+resource "aws_vpc" "shared" {
+  cidr_block = "172.16.0.0/16"
+  tags       = { Name = "${lterraform.workspace} Shared VPC"})
 }
+
+# resource "aws_vpc" "this" {
+#   cidr_block = local.config.cidr_block
+#   tags       = merge(var.tags, local.config.tags, { Name = "${local.config.prefix} VPC"})
+# }
