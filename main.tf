8d5c87470b0fa5304313d4bc6e9d59931becebbd
Migara Ekanayake <2110772+migara@users.noreply.github.com>
2021-03-22 12:06:22 +0000

    Destroying Dev

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
-resource "aws_vpc" "shared" {
-  cidr_block = "172.16.0.0/16"
-  tags       = { 
-    Name = "${terraform.workspace} Shared VPC"
-    Event = "Regional Summit 2021"
-  }
-}
+# resource "aws_vpc" "shared" {
+#   cidr_block = "172.16.0.0/16"
+#   tags       = { 
+#     Name = "${terraform.workspace} Shared VPC"
+#     Event = "Regional Summit 2021"
+#   }
+# }
 
-locals {
-  config = yamldecode(file("./env/${terraform.workspace}/config.yml"))
-}
+# locals {
+#   config = yamldecode(file("./env/${terraform.workspace}/config.yml"))
+# }
 
-resource "aws_vpc" "this" {
-  cidr_block = local.config.cidr_block
-  tags       = merge(var.tags, local.config.tags, { Name = "${local.config.prefix} VPC"})
-}
+# resource "aws_vpc" "this" {
+#   cidr_block = local.config.cidr_block
+#   tags       = merge(var.tags, local.config.tags, { Name = "${local.config.prefix} VPC"})
+# }
